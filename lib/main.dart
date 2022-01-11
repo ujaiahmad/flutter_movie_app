b3d03ab2abb650a704ce4854f32afb3a6fba0cff
chaizafran97 <82008745+chaizafran97@users.noreply.github.com>
2022-01-11 20:37:15 +0800

    Starting to add Firebase Auth
    Adding dependencies of Firebase Auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_movie_app/movie%20by%20popularity/movie_popularity_widget.dart';
 import 'package:flutter_movie_app/movie%20by%20year/movie_year_widget.dart';
+import 'package:firebase_auth/firebase_auth.dart'; // new
+import 'package:firebase_core/firebase_core.dart'; // new
+import 'package:provider/provider.dart'; // new
+import 'firebase_options.dart'; // new
+import 'src/authentication.dart'; // new
+import 'src/widgets.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -32,9 +38,9 @@ class _MainPageState extends State<MainPage> {
               Tab(
                 child: Text('Popular Movies'),
               ),
-              // Tab(
-              //   child: Icon(Icons.arrow_circle_up_outlined),
-              // )
+              Tab(
+                child: Text('Login/Sign Up'),
+              )
             ]),
           ),
           body: TabBarView(
