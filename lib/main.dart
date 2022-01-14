11bbae89b1664366447f4c0273b75b85ef34b70b
Ahmad Huzaifi Bin Ahmad Azahri <ujaiahmad@gmail.com>
2022-01-14 17:31:20 +0800

    comment the imports and change the length of default controller to 3

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,9 @@ import 'package:flutter_movie_app/movie%20by%20year/movie_year_widget.dart';
 import 'package:firebase_auth/firebase_auth.dart'; // new
 import 'package:firebase_core/firebase_core.dart'; // new
 import 'package:provider/provider.dart'; // new
-import 'firebase_options.dart'; // new
-import 'src/authentication.dart'; // new
-import 'src/widgets.dart';
+// import 'firebase_options.dart'; // new
+// import 'src/authentication.dart'; // new
+// import 'src/widgets.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -25,7 +25,7 @@ class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-        length: 2,
+        length: 3,
         child: Scaffold(
           appBar: AppBar(
             backgroundColor: Colors.purple[600],
