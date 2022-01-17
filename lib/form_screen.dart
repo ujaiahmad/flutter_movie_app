b3ff29c182740cc15a26ec9bcc6708be7bd07848
USMAN SENG <61685876+USMAN-SENG@users.noreply.github.com>
2022-01-17 10:38:34 +0800

    added a form screen

---
+++ lib/form_screen.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+
+class FormScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return FormScreenState();
+  }
+}
+
+class FormScreenState extends State<FormScreen> {
+  String _name = '';
+  String _email = '';
+  String _password = '';
+  String _url = '';
+  String _phoneNumber = '';
+  String _calories = '';
+
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  
+  Widget _buildEmail() {
+    return TextFormField(
+      decoration: InputDecoration(labelText: 'Email'),
+      validator: ( value) {
+        if (value!.isEmpty) {
+          return 'Email is Required';
+        }
+
+        if (!RegExp(
+                r"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")
+            .hasMatch(value)) {
+          return 'Please enter a valid email Address';
+        }
+
+        return null;
+      },
+      onSaved: ( value) {
+        _email = value!;
+      },
+    );
+  }
+
+  Widget _buildPassword() {
+    return TextFormField(
+      decoration: InputDecoration(labelText: 'Password'),
+      keyboardType: TextInputType.visiblePassword,
+      validator: ( value) {
+        if (value!.isEmpty) {
+          return 'Password is Required';
+        }
+
+        return null;
+      },
+      onSaved: ( value) {
+        _password = value!;
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Form Demo")),
+      body: SingleChildScrollView(
+        child: Container(
+          margin: EdgeInsets.all(24),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                
+                _buildEmail(),
+                _buildPassword(),
+                
+                SizedBox(height: 100),
+                RaisedButton(
+                  child: Text(
+                    'Submit',
+                    style: TextStyle(color: Colors.blue, fontSize: 16),
+                  ),
+                  onPressed: () {
+                    if (!_formKey.currentState!.validate()) {
+                      return;
+                    }
+
+                    _formKey.currentState!.save();
+
+                    
+                    print(_email);
+                    print(_password);
+                    
+
+                    //Send to API
+                  },
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
