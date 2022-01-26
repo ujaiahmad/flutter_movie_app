80ef72a62251d2a3c24d25d5c59161c47b0c7b07
chaizafran97 <82008745+chaizafran97@users.noreply.github.com>
2022-01-26 21:30:14 +0800

    .

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,14 @@ import 'package:provider/provider.dart';
 import 'auth.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+      options: FirebaseOptions(
+          apiKey:
+              'AAAALyPyB3U:APA91bHsIgwO1HzJR3dwz7h1TDHqsxEFRY35HDqY5lmz3jHXXlz4PYxX1W6BIqG3Jttkiej_WBfyqW9Bzn5wKYgN5iYXNNTZFgvPSWk1YA1gpeWmxy2eM1dV5lBMkd6uWRURrd-55nut',
+          appId: '1:202466527093:ios:81a62b19c309dda593753c',
+          messagingSenderId: '202466527093',
+          projectId: 'flutter-movie-app-aa745'));
   runApp(MyApp());
 }
 
